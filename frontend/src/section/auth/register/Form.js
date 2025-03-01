ed5d523cf433fa4bf6a7e691eeebeea8cb21a4f1
kamyab <acpersian@gmail.com>
2025-03-01 15:36:32 +0330

    install zod and react-hook-form and create validation for register form

--- frontend/src/section/auth/register/Form.js
+++ frontend/src/section/auth/register/Form.js
@@ -1,43 +1,28 @@
 'use client'
 
-import React, { useState } from 'react'
-import { toast, ToastContainer } from 'react-toastify'
+import React from 'react'
+import { toast } from 'react-toastify'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { registerValidation } from '@/utils/validator/registerValidation'
 
 export default function Form() {
   const usernameInput = React.useRef(null)
-  const emailInput = React.useRef(null)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(registerValidation) })
 
-  const [registerForm, setRegisterForm] = useState({
-    username: '',
-    email: '',
-    password: '',
-  })
-  const [repeatPassword, setRepeatPassword] = useState('')
-
-  React.useEffect(() => {
-    if (emailInput.current) {
-      registerForm.email.length > 0
-        ? (emailInput.current.style.direction = 'ltr')
-        : (emailInput.current.style.direction = 'rtl')
-    }
-  }, [registerForm.email])
-  React.useEffect(() => {
-    if (usernameInput.current) {
-      registerForm.username.length > 0
-        ? (usernameInput.current.style.direction = 'ltr')
-        : (usernameInput.current.style.direction = 'rtl')
-    }
-  }, [registerForm.username])
-
-  const handleRegister = () => {
-    if (registerForm.password === repeatPassword) {
+  const onSubmit = data => {
+    if (data.password === repeatPassword) {
     } else {
       toast('رمز عبور یکسان نمی باشد')
     }
   }
 
   return (
-    <div className="flex flex-col gap-6">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
       <div className="flex flex-col">
         <label
           className="text-stone-600 font-medium text-sm mb-1 cursor-pointer"
@@ -48,33 +33,34 @@ export default function Form() {
         <input
           id="usernameInput"
           ref={usernameInput}
-          value={registerForm.username}
-          onChange={e =>
-            setRegisterForm(prev => ({ ...prev, username: e.target.value }))
-          }
+          {...register('username')}
           type="text"
           className="p-1.5 font-medium rounded-md outline-none border border-gray-200 focus:border-gray-500 transition-all duration-200"
           placeholder="نام کاربری را وارد کنید"
         />
+        {errors.username && (
+          <small className="text-red-500 pt-1">
+            {errors.username?.message}
+          </small>
+        )}
       </div>
       <div className="flex flex-col">
         <label
           className="text-stone-600 font-medium text-sm mb-1 cursor-pointer"
-          htmlFor="usernameInput"
+          htmlFor="emailInput"
         >
           ایمیل
         </label>
         <input
-          ref={emailInput}
-          value={registerForm.email}
-          onChange={e =>
-            setRegisterForm(prev => ({ ...prev, email: e.target.value }))
-          }
+          {...register('email')}
           id="emailInput"
-          type="email"
+          type="text"
           className="p-1.5 font-medium rounded-md outline-none border border-gray-200 focus:border-gray-500 transition-all duration-200"
           placeholder="ایمیل خود را وارد کنید"
         />
+        {errors.email && (
+          <small className="text-red-500 pt-1">{errors.email?.message}</small>
+        )}
       </div>
       <div className="flex flex-col">
         <label
@@ -86,36 +72,38 @@ export default function Form() {
         <input
           id="passInput"
           type="password"
-          value={registerForm.password}
-          onChange={e =>
-            setRegisterForm(prev => ({ ...prev, password: e.target.value }))
-          }
+          {...register('password')}
           dir="ltr"
           className="p-1.5 font-medium rounded-md outline-none border border-gray-200 focus:border-gray-500 transition-all duration-200"
           placeholder=""
         />
-        <a
-          href=""
-          className="text-yellow-600 hover:text-yellow-800 transition-all duration-200 text-xs mt-1 underline underline-offset-4"
-        ></a>
+        {errors.password && (
+          <small className="text-red-500 pt-1">
+            {errors.password?.message}
+          </small>
+        )}
       </div>
 
       <div className="flex flex-col">
         <label
           className="text-stone-600 font-medium text-sm mb-1 cursor-pointer"
-          htmlFor="passInput"
+          htmlFor="confirmPassInput"
         >
           تکرار رمز عبور
         </label>
         <input
-          value={repeatPassword}
-          onChange={e => setRepeatPassword(e.target.value)}
-          id="passInput"
+          {...register('confirmPassword')}
+          id="confirmPassInput"
           type="password"
           dir="ltr"
           className="p-1.5 font-medium rounded-md outline-none border border-gray-200 focus:border-gray-500 transition-all duration-200"
           placeholder=""
         />
+        {errors.confirmPassword && (
+          <small className="text-red-500 pt-1">
+            {errors.confirmPassword.message}
+          </small>
+        )}
         <a
           href=""
           className="text-yellow-600 hover:text-yellow-800 transition-all duration-200 text-xs mt-1 underline underline-offset-4"
@@ -124,7 +112,7 @@ export default function Form() {
 
       <div className="flex justify-center flex-col">
         <button
-          onClick={handleRegister}
+          type="submit"
           className="bg-yellow-300 hover:bg-yellow-500 transition-all duration-200 text-stone-800 w-full rounded-md p-2 text-sm font-medium"
         >
           ایجاد حساب
@@ -140,6 +128,6 @@ export default function Form() {
           </a>
         </div>
       </div>
-    </div>
+    </form>
   )
 }
