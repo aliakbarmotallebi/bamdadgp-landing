bb6aed6ccbf27c01b4fd3571d4944ddcd59c82c7
kamyab <acpersian@gmail.com>
2025-03-10 14:56:54 +0330

    hover scale card

--- frontend/src/components/section/store/ProductItem.js
+++ frontend/src/components/section/store/ProductItem.js
@@ -49,7 +49,7 @@ export default function ProductItem({ productItem }) {
         </a>
       </div>
       <div>
-        <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm inline-block">
+        <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm inline-block">
           {productItem.product_category.cat_title}
         </span>
       </div>
