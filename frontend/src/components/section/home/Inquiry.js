80d9f5fa00433078452a4c6feb013182667e255e
aliakbar moatllebi <motallebi.aliakbar@gmail.com>
2025-04-29 04:28:31 +0330

    btn loading

--- frontend/src/components/section/home/Inquiry.js
+++ frontend/src/components/section/home/Inquiry.js
@@ -93,7 +93,7 @@ export default function Inquiry() {
           className="bg-yellow-300 w-64 lg:w-auto hover:bg-yellow-400 transition-all duration-200 text-stone-800 py-2.5 px-6 rounded-md flex justify-center items-center"
         >
           {loading ? (
-            <svg className="size-5 animate-spin text-stone-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+            <svg className="size-5 animate-spin text-stone-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
           ) : (
             'استعلام و فعالسازی گارانتی'
           )}
